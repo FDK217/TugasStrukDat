9eec99d70a640a60740b63edc263e20a8f5d5908
Fadhilla Dafa Karunia <dafakaruniafadhilla@gmail.com>
2025-03-03 11:43:44 +0700

    Upload tugas

---
+++ .cphcsrun/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace tugas
+{
+    class Program2
+    {
+        static void Main()
+        {
+            Console.Write("Masukkan angka: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Angka yang Anda masukkan adalah: " + input);
+        }
+    }
+}
