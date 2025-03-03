9863fdb60b93a8df65f59149e39e4eaadbf6ce02
Fadhilla Dafa Karunia <dafakaruniafadhilla@gmail.com>
2025-03-03 10:26:18 +0700

    Commit tugas

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int pilihan;
+Console.Write("Masukkan angka: ");
+pilihan = Convert.ToInt32(Console.ReadLine());
+
+if (pilihan > 1)
+{
+    Console.WriteLine("Angka anda lebih besar dari 1");
+}
+else if (pilihan == 1)
+{
+    Console.WriteLine("Angka anda adalah 1");
+}
+else
+{
+    Console.WriteLine("Angka anda lebih kecil dari 1");
+}
